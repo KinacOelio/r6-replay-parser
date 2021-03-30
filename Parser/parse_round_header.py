064dc6e934b01baea5de72010d48f033fa35637b
natoast <ntw6232@wmich.edu>
2021-03-30 14:29:35 -0400

    second segment

--- Parser/parse_round_header.py
+++ Parser/parse_round_header.py
@@ -23,17 +23,21 @@ def bytes_to_hex_string(hex_bytes):
     return ' '.join([hex_str[i:i + 4] for i in range(0, len(hex_str), 4)])
 
 
-def print_block(hex_bytes, explanation, args):
-    print("--------------------------" * 5)
-    if not args.hide_source_bytes:
-        hex_string = bytes_to_hex_string(hex_bytes)
-        if len(hex_string) > (args.column_width - 10):
-            hex_string = hex_string[:args.column_width - 13] + "..."
+def print_block(hex_bytes, explanation, args, segment):
+    if segment == 0:
+        print("--------------------------" * 5)
+        if not args.hide_source_bytes:
+            hex_string = bytes_to_hex_string(hex_bytes)
+            if len(hex_string) > (args.column_width - 10):
+                hex_string = hex_string[:args.column_width - 13] + "..."
 
-        print(hex_string, end='')
-        print(' ' * (args.column_width - len(hex_string)), end='')
+            print(hex_string, end='')
+            print(' ' * (args.column_width - len(hex_string)), end='')
 
-    print(explanation)
+        print(explanation)
+    elif segment == 1:
+        print("--------------------------" * 5)
+        print(bytes_to_hex_string(hex_bytes))
 
 
 def read_string(bytestream):
@@ -70,15 +74,15 @@ def main(args):
         magic_number = f.read(12)
 
         if args.show_header:
-            print_block(magic_number, ".REC File Magic Number", args)
+            print_block(magic_number, ".REC File Magic Number", args, 0)
         str_bytes, str_val = read_string(f)
         if args.show_header:
-            print_block(str_bytes, f'"{str_val}"', args)
+            print_block(str_bytes, f'"{str_val}"', args, 0)
 
         unknown_header_bytes = f.read(8)
 
         if args.show_header:
-            print_block(unknown_header_bytes, "????? (Purpose unknown)", args)
+            print_block(unknown_header_bytes, "????? (Purpose unknown)", args, 0)
 
         number_of_header_properties_bytes = f.read(8)
 
@@ -88,7 +92,7 @@ def main(args):
             print_block(
                 number_of_header_properties_bytes,
                 f"{number_of_header_properties} key-value string pairs follow...",
-                args
+                args, 0
             )
 
         for i in range(number_of_header_properties):
@@ -98,34 +102,41 @@ def main(args):
             header_properties.append((key_str, val_str))
 
             if args.show_header:
-                print_block(key_bytes + val_bytes, f'"{key_str}": "{val_str}"', args)
+                print_block(key_bytes + val_bytes, f'"{key_str}": "{val_str}"', args, 0)
 
-        if args.show_body and args.show_header:
+        for segment in range(args.show_body):
             print("--------------------------" * 5)
             print("--------------------------" * 5)
-            print("FILE BODY FOLLOWS")
+            print("FILE SEGMENT FOLLOWS")
             print("--------------------------" * 5)
 
-        number_of_packets_bytes = f.read(8)
-        number_of_packets = decode_integer(number_of_packets_bytes)
-        if args.show_body:
-            print_block(number_of_packets_bytes, f"{number_of_packets} packets follow...", args)
-
-        zeros = f.read(8)
-        if args.show_body:
-            print_block(zeros, "Zeros to space between header and body?", args)
-
-        for i in range(number_of_packets - 1):
-            packet_bytes, packet_desc = read_packet(f, i)
+            number_of_packets_bytes = f.read(4)
+            number_of_packets = decode_integer(number_of_packets_bytes)
             if args.show_body:
-                print_block(packet_bytes, packet_desc, args)
+                print_block(number_of_packets_bytes, f"{number_of_packets} packets follow...", args, 0)
 
-            if i > args.max_packets:
-                return
 
-        zeros = f.read(8)
-        if args.show_body:
-            print_block(zeros, "Zeros to space between body and footer?", args)
+            if segment == 0:
+                zeros = f.read(12)
+                if args.show_body:
+                    print_block(zeros, "Zeros to space between header and body?", args, 0)
+
+            for i in range(number_of_packets - 1):
+                if segment == 0:
+                    packet_bytes, packet_desc = read_packet(f, i)
+                if segment == 1:
+                    packet_bytes = f.read(29)
+                if i > args.max_packets:
+                    continue
+                if args.show_body:
+                    print_block(packet_bytes, packet_desc, args, segment)
+
+                if i > args.max_packets:
+                    return
+
+            zeros = f.read(8)
+            if args.show_body:
+                print_block(zeros, "Zeros to space between segments", args, 0)
 
 
 if __name__ == "__main__":
@@ -133,7 +144,7 @@ if __name__ == "__main__":
     parser.add_argument('file_path', metavar='path', type=str, help="The file to parse")
     parser.add_argument('-m', '--max-packets', default=200, type=int)
     parser.add_argument('-H', '--show-header', action='store_true')
-    parser.add_argument('-B', '--show-body', action='store_true')
+    parser.add_argument('-B', '--show-body', action='store', default=0, type=int, help="the number of body segments to display (default=0)")
     parser.add_argument('-S', '--hide-source-bytes', action='store_true')
     parser.add_argument('-c', '--column-width', default=70, type=int)
 
